2b2b632a2e02219c20b192dd9cd70fec29c28543
Miquel Sonsona <miquelsonsona@gmail.com>
2014-09-08 08:26:55 +0200

    independent css
    Moved style definition to a separated css file, included from the
    manifest.json

--- identify.js
+++ identify.js
@@ -3,14 +3,14 @@ $(function() {
     function(){
       var coupon_id = $(this).data('id');
       if (typeof coupon_id != 'undefined') {
-        $(this).prepend('<span style="background: #66c13a; position: fixed; bottom: 0; right: 0; z-index: 10000000;">coupon_id ' + coupon_id + '</span>');
-        $(this).css("border", "1px #66c13a solid");
+        $(this).prepend('<span class="identify-display">coupon_id ' + coupon_id + '</span>');
+        $(this).addClass('identify-coupon');
       }
     }, function() {
       var coupon_id = $(this).data('id');
       if (typeof coupon_id != 'undefined') {
         $(this).children().first().remove();
-        $(this).css("border", "");
+        $(this).removeClass('identify-coupon');
       }
     }
   );
